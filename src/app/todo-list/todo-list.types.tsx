5b06073545ff12b355c519bc407bbcd1071f9956
toaney <toaney@gmail.com>
2024-03-19 10:41:22 -0700

    fitbod project setup create design layout header, body, footer sections

--- src/app/todo-list/todo-list.types.tsx
+++
@@ -1,9 +0,0 @@
-type AddItem = (newTodo: string) => void;
-type DeleteItem = (id: string) => void;
-type ToggleItem = (id: string) => void;
-
-type TodoItemInterface = {
-  id: string;
-  value: string;
-  completed: boolean;
-}
